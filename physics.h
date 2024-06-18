790b28fd63e531d070ea83de8581c46b2be2fb46
Yellowstrawberrys <dirjhhsg@gmail.com>
2024-06-18 11:44:17 +0900

    70%

---
+++ physics.h
@@ -0,0 +1,23 @@
+//
+// Created by 정은수 on 2024. 6. 18..
+//
+
+#ifndef PHYSICS_H
+#define PHYSICS_H
+#include "raylib.h"
+
+typedef struct _physicsObject {
+    int id;
+    Vector2 coordinate, force;
+    int facing;
+
+    void (*onCollide)(struct _physicsObject*);
+
+    float weight;
+} PhysicsObject;
+
+void updatePhysics();
+PhysicsObject* generatePhysicsObject();
+void unregisterPhysicsObject(PhysicsObject* object);
+
+#endif //PHYSICS_H
