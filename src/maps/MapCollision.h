9ff67428c8c195a19b178dcadf5adcd55013571b
Ryu_HanSeok <ryuhskyle@gmail.com>
2024-06-27 03:56:38 +0900

    map_collision_lib_init

---
+++ src/maps/MapCollision.h
@@ -0,0 +1,25 @@
+#ifndef MAP_COLLISION_H
+#define MAP_COLLISION_H
+#include <stdbool.h>
+#include "./include/raylib.h"
+#include "include/tmx.h"
+
+
+typedef struct CollisionBoxes {
+    int amountCollisionBoxes;
+    Rectangle *scaledCollision;
+    float scale;
+} CollisionBoxes;
+
+/// @brief Initialize rectangles for every collision in all tiles of specific layer
+/// @param tmxFileName The TMX file
+/// @param origin Origin for scaling and positioning of the rectangles
+/// @param scale Multiplacator for scaling the rectangles
+/// @param resource_manager tmx_resource_manager
+/// @param layerId The layer where the collision data is placed
+/// @return Array with all rectangles
+CollisionBoxes* initCollisionBoxes(const char* tmxFileName, Vector2 origin, float scale, tmx_resource_manager* resource_manager, int layerId);
+void unloadMapCollision(CollisionBoxes *collisionBoxes);
+
+
+#endif
